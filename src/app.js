3bc2025b38ec27078d71efba4b7c1eb9ac308cd7
Abhilash <abhilashbangera97@gmail.com>
2024-12-24 17:28:04 +0530

    Exploring Get,Put,Patch,Post,Delete.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,34 @@ const express = require ("express");
 
 const app = express();
 
-app.use((req,res) => {
-    res.send("Helloooooooooooo Yoooooo")
+app.use("/user",(req,res) => {
+    res.send("Yo Yo Yo Yooooooooo");
+});
+
+app.post("/user", (req,res) => {
+    res.send("Data saved successfully!");
+
+});
+
+app.patch("/user",(req,res) =>{
+    res.send("Data updated successfully");
+
+});
+
+app.delete("/user",(req,res) => {
+    res.send("Data deleted successfully");
+});
+
+app.get("/user",(req,res) =>{
+    res.send({firstName:"Abhilash",lastName:"Yoo"});
+});
+
+app.get("/",(req,res) => {
+    res.send("Helloooooooooooo")
+});
+
+app.get("/hello", (req,res) => {
+    res.send("Yoooooo")
 });
 
 app.listen(3000, () => {
