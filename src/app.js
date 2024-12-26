1c185dedfa3758a7fbc23d2da7636c32d1a13771
Abhilash <abhilashbangera97@gmail.com>
2024-12-26 15:04:46 +0530

    Worked with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,35 +2,24 @@ const express = require ("express");
 
 const app = express();
 
-app.use("/user",(req,res) => {
-    res.send("Yo Yo Yo Yooooooooo");
-});
+app.use(
+    "/user",
+    (req,res,next) => {
+        console.log("Handling the route user 1");
+        // res.send("Yo Yo ");
+        next();
+    },
+    (req,res,next) => {
+        console.log("Handling the route user 2");
+        // res.send("Yo Yo Yooooooooo");
+        next();
+    },
+    (req,res) => {
+        console.log("Handling the route user 3");
+        res.send("Yo Yo Yooo Noooooo");
+    }
+);
 
-app.post("/user", (req,res) => {
-    res.send("Data saved successfully!");
-
-});
-
-app.patch("/user",(req,res) =>{
-    res.send("Data updated successfully");
-
-});
-
-app.delete("/user",(req,res) => {
-    res.send("Data deleted successfully");
-});
-
-app.get("/user",(req,res) =>{
-    res.send({firstName:"Abhilash",lastName:"Yoo"});
-});
-
-app.get("/",(req,res) => {
-    res.send("Helloooooooooooo")
-});
-
-app.get("/hello", (req,res) => {
-    res.send("Yoooooo")
-});
 
 app.listen(3000, () => {
     console.log("Sever is successfully listeing on port number 3000...");
