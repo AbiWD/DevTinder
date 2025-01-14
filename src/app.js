fa81002ca3d74dafd21a9edf5e19bb23aa77a186
Abhilash <abhilashbangera97@gmail.com>
2025-01-14 17:12:47 +0530

    Database-init

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+
+app.post("/signup", async (req,res) => {
+  // Creating a new instance of the User model
+  const user = new User({
+    firstName: "Akshay",
+    lasrName:"Saini",
+    emailId:"akshaysaini@gmail.com",
+    password: "akshay@123",
+  });
+
+  try {
+    await user.save();
+    res.send ("User added successfully.");
+  }catch(err){
+    res.status(400).send("Error saving the user"+err.message);
+  }  
+});
+
 
 connectDB()
 .then(() => {
