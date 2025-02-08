c63e33a86bfce8cecb0a2537a96c79e21bd84b5e
Abhilash <abhilashbangera97@gmail.com>
2025-02-08 16:04:18 +0530

    using express routers

---
+++ Apilist.md
@@ -0,0 +1,34 @@
+# DevTinder API
+
+## Autorouter:>>>>>
+
+-POST/signup
+-POST/login
+-POST/logout
+
+
+## ProfileRouter:>>>>
+
+-GET/profile/view
+-PATCH/profile/edit
+-PATCH/profile/password
+
+
+## ConnectionRequestRouter:>>>>>
+
+-POST/request/send/intrested/:userId
+-POST/requestsend/ignored/:userId
+-POST/request/review/accepted/:requestId
+-POST/request/review/rejected/:requestId
+
+## user router:>>>>
+-GET/user/connnections
+-GET/user/request 
+-GET/user/feed - Gets yout the profiles of other users on platform 
+
+
+
+
+
+
+Status: ignore, intrested, accepted, rejected.
--- src/app.js
+++ src/app.js
@@ -1,163 +1,21 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user");
-const {validateSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const {userAuth} = require("./middlewares/auth");
+
 
 app.use(express.json());
 app.use(cookieParser());
 
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-app.post("/signup", async (req,res) => {
-  //Validation of data
-  validateSignUpData (req); 
-  
-  const {firstName, lastName, emailId,password} = req.body;
-
-  //Encrytpt the password
-  const passwordHash = await bcrypt.hash(password,10);
- 
-
-  // Creating a new instance of the User model
-
-  const user = new User({
-    firstName, lastName,emailId, password: passwordHash
-  });
-
-  try {
-    await user.save();
-    res.send ("User added successfully.");
-  }catch(err){
-    res.status(400).send("ERROR : "+err.message);
-  }  
- });
-
-app.post("/login",async (req, res) =>{
-  try{
-    const {emailId,password} = req.body;
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
 
 
-    const user = await User.findOne({emailId: emailId});
-    if(!user){
-      throw new Error("Invalid credentials");
-    }
-    
-    const isPasswordValid = await user.validatePassword(password);
-
-    if(isPasswordValid){
-      // Create a JWT token
-
-      const token = await user.getJWT();
-
-      // Add the token to cookie and send the response  back to user
-      res.cookie("token", token,{ httpOnly:true });
-
-      res.send("Login Successful!!");
-    }else{
-      throw new Error ("Invalid credentials");
-    }
-
-
-  }catch(err){
-    res.status(400).send("ERROR: "+ err.message); 
-  }
-});
-
-
-app.get("/profile", userAuth, async (req,res) =>{
-
-  try{
-  
-  const  user = req.user;
-
-  res.send(user);}
-  catch(err){
-  res.status(400).send("ERROR: "+ err.message); 
-  }
-});
-
-app.post("/sendConnectionRequest",userAuth, async(req,res) =>{
-  const user =  req.user;
-  //Sending a connection request 
-  console.log("Sending a conneciton request");
-  res.send(user.firstName+" sent the connection request");
-});
-
-
- // Get user by email>>>>>
- app.get("/user", async (req, res) =>{
-  const userEmail = req.body.emailId;
-   
-  try{
-    const user = await User.find({emailId: userEmail});
-    res.send(user);
-  }
-  catch(err){
-    res.status(400).send("Something went wrong!");
-  }
- });
-
-
- //Feed Api - Get / feed - get all the users from the database
- app.get("/feed",async (req, res) =>{
-
-  try {
-    const users = await User.find({});
-    res.send(users);
-  }catch(err){
-    res.status(400).send("Something went wrong!");
-  }
- });
-
-//Delete user api>>>>> Delete user by Id >>>>>>..
-app.delete("/user", async (req, res) => {
-
-  const userId = req.body.userId;
-  try{
-    const user = await User.findByIdAndDelete(userId);
-
-    res.send("User deleted successfully");
-
-  }catch(err){
-    res.status(400).send("Something went wrong!");
-  }
-})
-
-
-//Update data of User>>>>>>>>>>
-app.patch("/user/:userId",async (req,res) =>{
-  const userId = req.params?.userId;
-  const data = req.body;
-
-
-  try{
-    const ALLOWED_UPDATES = [
-      "photoUrl","about","gender","age","skills",
-    ];
-  
-    const isUpdateAllowed = Object.keys(data).every((k) => 
-    ALLOWED_UPDATES.includes(k)
-    );
-    if(!isUpdateAllowed){
-      throw new Error ("Update not allowed");
-    }
-    if(data?.skills.length > 10 ){
-      throw new Error("Skills cannot be more than 10")
-    }
-
-    await User.findByIdAndUpdate({_id: userId}, data,{
-      returnDocument:"after",
-      runValidators: true,
-    }); 
-    res.send("User updated successfully");
-  }catch(err){
-    res.status(400).send("Update failed!" + err.message);
-  }
-});
 
 
 connectDB()
