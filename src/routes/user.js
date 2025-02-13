8bc25436fd6c2bcce95a2951a319ca8b0e43fa48
Abhilash <abhilashbangera97@gmail.com>
2025-02-13 16:59:20 +0530

    Update user api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,50 +2,63 @@ const express = require("express");
 const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
-const connectionRequest = require("../models/connectionRequest");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-const USER_SAFE_DATA = "firstName lastName photoUrl gender age skills";
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
-//Get all the pending connection request for the loggedIn user
-userRouter.get("/user/requests/received",userAuth, async (req,res) =>{
-    try{
+// Get all the pending connection request for the loggedIn user
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
 
-        const connectionRequest = await connectionRequest.find({
+        const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
-
+            status: "interested",
         }).populate("fromUserId", USER_SAFE_DATA);
+    // }).populate("fromUserId", ["firstName", "lastName"]);
 
+     res.json({
+        message: "Data fetched successfully",
+        data: connectionRequests,
+    });
 
-     res.json({message: "Data sent successfully", data: connectionRequests});
-
-    }catch(err){
-        req.statusCode(400).send("ERROR: "+ err.message);
+    } catch (err) {
+        req.statusCode(400).send("ERROR: " + err.message);
     }
 });
 
 
-userRouter.get("/user/connection",userAuth,async(req,res) =>{
-    try{
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
 
-        const connectionRequests = await connectionRequest.find({
+        const connectionRequests = await ConnectionRequest.find({
             $or: [
-                {toUserId: loggedInUser._id, status: "accepted"},
-                {fromUserId: loggedInUser._id, status: "accepted"},
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
             ],
-        }).populate("fromUserId",USER_SAFE_DATA);
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-        const data = connectionRequests.map((row) => row.fromUserId);
-        
-        res.json({ data });
+    console.log(connectionRequests);
 
-    }catch(err){
-        res.status(400).send({message: err.message});
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+
+    } catch (err) {
+        res.status(400).send({ message: err.message });
     }
 });
 
 
 
 
-moduel.exprorts = userRouter;
+module.exports = userRouter;
