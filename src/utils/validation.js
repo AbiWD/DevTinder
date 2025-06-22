a642b3c1e2a364485dcd95363035fc5530f9503d
Abhilash <137391776+AbiWD@users.noreply.github.com>
2025-06-22 16:07:06 +0530

    One last commit

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,31 +1,36 @@
 const validator = require("validator");
 
-const validateSignUpData = (req) =>{
-    
-    const {firstName, lastName, emailId, password} = req.body;
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
 
-    if (!firstName || !lastName){
-        throw new Error("Enter a valid name!");
-    }
-    else if (!validator.isEmail(emailId)){
-        throw new Error("Email is not valid");
-    }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter a strong password!");
-    }
+  if (!firstName || !lastName) {
+    throw new Error("Enter a valid name!");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password!");
+  }
 };
 
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "age",
+    "about",
+    "skills",
+  ];
 
-const validateEditProfileData = (req) =>{
-   const allowedEditFields = ["firstName","lastName","emailId","photoUrl","age","about","skills"];
-   
-   const isEditAllowed = Object.keys(req.body).every((field) => 
-    allowedEditFields.includes(field));
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
 
-   return isEditAllowed;
-
+  return isEditAllowed;
 };
 
 module.exports = {
-    validateSignUpData , validateEditProfileData
-}
+  validateSignUpData,
+  validateEditProfileData,
+};
